b47b49314b08e697c0631752e0ed47dd9a7b5f44
Shu Yoshioka <yoshioka63@gmail.com>
2018-10-08 23:00:42 +0900

    Add sample api

---
+++ layouts/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Vue on Rails!! Imejin</h1>
+  </div>
+</template>
+
+<script>
+import 'regenerator-runtime/runtime'
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+    }
+  },
+  async created () {
+    const res = await axios.get('localhost:3000/api')
+    this.code = res.code
+    this.message = res.message
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="sass">
+h1 {
+  color: blue;
+}
+</style>
